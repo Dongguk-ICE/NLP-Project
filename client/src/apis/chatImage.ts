a33376ffca420d4f4a6a2e599204b4c66cb33a29
Choijian <an032944@daum.net>
2024-06-10 12:14:58 +0900

    Fix: form-data 객체를 반환하도록 변경

--- client/src/apis/chatImage.ts
+++ client/src/apis/chatImage.ts
@@ -1,17 +1,16 @@
 import { instance } from "./axios";
 
-export const chatImage = async (formData) => {
-  const response = await instance.post(
-    "/docent/chatting/",
-    {
-      image: formData,
+export const chatImage = async (file) => {
+  // FormData 객체 생성
+  const formData = new FormData();
+  // FormData 객체에 이미지 파일 추가 ('image'는 서버에서 기대하는 필드 이름)
+  formData.append("image", file);
+
+  const response = await instance.post("/docent/chatting/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
     },
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+  });
   console.log(response);
   return response.data;
 };
